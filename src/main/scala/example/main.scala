52ce0241b3cbb3b88807048021e7092d8966ebdc
Aliaksei Karatkevich <abk@krononsoft.com>
2023-09-05 11:00:48 +0300

    functions-sum-and-max

---
+++ src/main/scala/example/main.scala
@@ -0,0 +1,9 @@
+package example
+//@main def run(): Unit =
+//  println(Lists.max(List(1,3,2)))
+
+//@main def run(args: Array[String]) =
+//  println("Hello world")
+
+object Hello extends App:
+  println("Hello World")
